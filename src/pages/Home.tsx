cd801fd63ed29bda87ce968d9812368a68956b32
cacara82 <cacara890@gmail.com>
2025-05-17 19:00:33 +0200

    gap fix first ver

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,11 +7,11 @@ export default function Home() {
             <Navbar />
             <div className="w-full h-screen relative overflow-hidden">
                 <img src={"/hero.jpg"} alt="Banner" className="w-full h-full object-cover" />
-                <div className="flex flex-col absolute inset-0 flex items-center justify-center px-4" style={{ paddingBottom: '8rem' }}>
+                <div className="flex flex-col mt-10 absolute inset-0 flex items-center justify-center px-4" style={{ paddingBottom: '8rem' }}>
                     <div className="relative z-10 bg-white bg-opacity-60 rounded-lg p-6 md:p-8 backdrop-blur-sm max-w-lg mx-auto">
                         <img src="banner.png" alt="INNOVATE PRO BONO" />
                     </div>
-                    <div>
+                    <div className="mt-6">
                         <SeeBelowButton />
                     </div>
                 </div>
