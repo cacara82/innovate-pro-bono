6552757f91cd16bf1b9e4e89d70c4a7ac6eb16ff
cacara82 <cacara890@gmail.com>
2025-10-01 17:21:01 +0200

    changed footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,16 +1,16 @@
 export default function Footer() {
     return (
         <footer className="w-full bg-[#031f3d] text-gray-300 py-8 text-center">
-            <div className="max-w-3xl mx-auto px-4">
-                <p className="text-sm mb-3">
-                    &copy; {new Date().getFullYear()} Innovate Pro-Bono. Todos los derechos reservados. {/* Year by Date object to auto numerate */}
-                </p>
-                <nav className="space-x-4 text-xs">
+            <div className="max-w-3xl mx-auto px-4 mb-5">
+                <nav className="space-x-4 text-sm">
                     <a href="/legal" className="hover:text-white hover:underline">Política de Privacidad</a>
                     <a href="/legal" className="hover:text-white hover:underline">Aviso Legal</a>
                     <a href="mailto:cacara890@gmail.com" className="hover:text-white hover:underline">Contacto</a>
                 </nav>
             </div>
+            <p className="text-xs"><strong>
+                &copy; {new Date().getFullYear()} Innovate Pro Bono & Asociados. Todos los derechos reservados. {/* Year by Date object to auto numerate */}
+            </strong></p>
         </footer>
     );
 }
